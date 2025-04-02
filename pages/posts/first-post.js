dc63a5a9ac4aa99fb493265a7c929a3395b579fd
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-04-02 19:44:22 +0800

    addedan layout

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Link from "next/link";
 import Layout from "../../components/layout";
+import Image from "next/image";
+import Script from "next/script";
 
 export default function FirstPost() {
   return (
@@ -8,7 +10,20 @@ export default function FirstPost() {
       <Head>
         <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg"
+        height={144}
+        width={144}
+        alt="Alden Derf"
+      />
       <h2>
         <Link href="/">← Back to home</Link>
       </h2>
