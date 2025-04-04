c3ba0ad3ad72c29f743f79f093efc7b5ee13b5a6
Alden Derf <aldenderfc.fabro99@gmail.com>
2025-04-04 22:24:54 +0800

    Polished the Post Page
    - added title of the page
    - Formatted the date
    - Added CSS

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,19 +1,25 @@
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
+import Head from "next/head";
+import Date from "@/components/date";
+import utilStyles from "../../styles/utils.module.css";
 
-export default function Post({postData}) {
-    console.log(postData)
-  return <Layout>
-    {postData.title}
-    <br/>
-
-    {postData.id}
-    <br />
-    {postData.date}
-    <br />
-        <div dangerouslySetInnerHTML={{__html: postData.contentHtml}} />
-    
-  </Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1> {postData.title}</h1>
+      </article>
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      <article />
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
